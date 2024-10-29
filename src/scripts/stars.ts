854f0b87859c175f7bc625e8b9123fb67757749a
Averyden <igo408gt@gmail.com>
2024-10-29 16:55:27 +0100

    remove unnecesarry code

--- src/scripts/stars.ts
+++ src/scripts/stars.ts
@@ -107,8 +107,6 @@ class Star {
     }
 
     draw(): void {
-        var chance // for the funny
-        chance = Math.random
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
         ctx.fillStyle = this.currentCol
