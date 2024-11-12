a01dadcc8e5fca58a35a99f35f6b84edbaef92d3
averyden <igo408gt@gmail.com>
2024-11-12 07:43:56 +0100

    Code rainbow handler

---
+++ src/scripts/Dependencies/rainbow.ts
@@ -0,0 +1,18 @@
+setInterval(rainbow, 1000);
+function rainbow() {
+    const toColor = document.getElementById('rainbow')
+    if (toColor != null) {
+        toColor.style.color = getRandomColor()   
+    } else {
+        console.log("No elements to rainbow.")
+    }
+}
+
+function getRandomColor() {
+    let charset = "0123456789ABCDEF";
+    let color = "#"
+    for (var i = 0; i<6; i++) {
+        color += charset[Math.floor(Math.random() * 16)];
+    }
+    return color
+}
