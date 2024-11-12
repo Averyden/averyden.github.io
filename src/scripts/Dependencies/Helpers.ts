3851630bfd012bb037f9e34b97b2c191c75df610
Averyden <igo408gt@gmail.com>
2024-11-12 23:00:13 +0100

    implement function that randomly selects a preview image/gif for notesu

--- src/scripts/Dependencies/Helpers.ts
+++ src/scripts/Dependencies/Helpers.ts
@@ -3,3 +3,22 @@
 function getRandomInt(min: number, max: number): number { // Fetch a random number in the range determined by the two parameters
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+interface INotesuPreview {
+    src: string;
+    caption: string;
+}
+
+function getRandomNotesuPreview() : INotesuPreview {
+    const collectionOfPreviews: INotesuPreview[] = [
+        { src: "assets/projectPreviews/notesuGifs/addingNotes.gif", caption: "creating notes in Notesu."},
+        { src: "assets/projectPreviews/notesuGifs/configDeadline.gif", caption: "configuring the deadline of a note in Notesu."}, 
+        { src: "assets/projectPreviews/notesuGifs/configDelete.gif", caption: "deleting a note in Notesu."},
+        { src: "assets/projectPreviews/notesuGifs/noteEditing.gif", caption: "editing a note in Notesu."},
+        { src: "assets/projectPreviews/notesuGifs/notesuStill.png", caption: "Notesu preview."} //? the one singular png :troll:
+    ] // this is a HORRID way of doing it. BUT IT WORKS SO FUCK OYU!
+
+    const index = Math.floor(Math.random() * collectionOfPreviews.length)
+    return collectionOfPreviews[index]
+}
+
