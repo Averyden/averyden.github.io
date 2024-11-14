4dd92d27df5c01da4e91bcb08202e5e530a9efc3
averyden <igo408gt@gmail.com>
2024-11-14 09:56:43 +0100

    Generalize preview selector

--- src/scripts/Dependencies/Helpers.ts
+++ src/scripts/Dependencies/Helpers.ts
@@ -4,22 +4,34 @@ function getRandomInt(min: number, max: number): number { // Fetch a random numb
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-interface INotesuPreview {
+interface IPicturePreview {
     src: string;
     caption: string;
 }
 
-function getRandomNotesuPreview() : INotesuPreview {
-    const collectionOfPreviews: INotesuPreview[] = [
-        { src: "/assets/projectPreviews/notesuGifs/addingNotes.gif", caption: "creating notes in Notesu."},
-        { src: "/assets/projectPreviews/notesuGifs/configDeadline.gif", caption: "configuring the deadline of a note in Notesu."}, 
-        { src: "/assets/projectPreviews/notesuGifs/configDelete.gif", caption: "deleting a note in Notesu."},
-        { src: "/assets/projectPreviews/notesuGifs/noteEditing.gif", caption: "editing a note in Notesu."},
-        { src: "/assets/projectPreviews/notesuGifs/notesuStill.png", caption: "Notesu preview."} //? the one singular png :troll:
-    ] // this is a HORRID way of doing it. BUT IT WORKS SO FUCK OYU!
+const projectPreviews: { [key: string]: IPicturePreview[] } = {
+    notesu: [
+        { src: "/assets/projectPreviews/notesu/addingNotes.gif", caption: "creating notes in Notesu."},
+        { src: "/assets/projectPreviews/notesu/configDeadline.gif", caption: "configuring the deadline of a note in Notesu."}, 
+        { src: "/assets/projectPreviews/notesu/configDelete.gif", caption: "deleting a note in Notesu."},
+        { src: "/assets/projectPreviews/notesu/noteEditing.gif", caption: "editing a note in Notesu."},
+        { src: "/assets/projectPreviews/notesu/notesuStill.png", caption: "Notesu preview."} //? the one singular png :troll:
+    ],
 
-    const index = Math.floor(Math.random() * collectionOfPreviews.length)
-    return collectionOfPreviews[index]
+    pasman: [
+        
+    ]
+} // this is a HORRID way of doing it. BUT IT WORKS SO FUCK OYU!
+
+function getRandomProjectPreview(project: string) : IPicturePreview | null {
+    const collectionOfPreviews = projectPreviews[project]
+    if (collectionOfPreviews && collectionOfPreviews.length > 0) {
+        const index = Math.floor(Math.random() * collectionOfPreviews.length)
+        return collectionOfPreviews[index]
+    } else {
+        console.log(`No previews were found for projectL ${project}...`)
+        return null;
+    }
 }
 
 function applySelectedNotesuPreview() : void {
