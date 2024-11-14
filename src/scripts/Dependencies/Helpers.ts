d63a1397faaad12a0a3f5a35c90fbb9c347e97e0
averyden <igo408gt@gmail.com>
2024-11-14 10:56:31 +0100

    Insert src and caption for new img

--- src/scripts/Dependencies/Helpers.ts
+++ src/scripts/Dependencies/Helpers.ts
@@ -21,7 +21,8 @@ const projectPreviews: { [key: string]: IPicturePreview[] } = {
     Pasman: [
         {src: "/assets/projectPreviews/pasman/pasManMainVault.png", caption: "Pasman main password vault that is completely empty."},
         {src: "/assets/projectPreviews/pasman/pasManRegisterInvalidMail.png", caption:"attempting to register an account in pasman, using an invalid email."},
-        {src: "/assets/projectPreviews/pasman/pasmanEmailFormatDetection.gif", caption:"demonstration of code being able to recognize a valid email format in pasman"}
+        {src: "/assets/projectPreviews/pasman/pasmanEmailFormatDetection.gif", caption:"demonstration of code being able to recognize a valid email format in pasman"},
+        {src: "/assets/projectPreviews/pasman/pasManComputerLeaked.gif", caption: "checking how many breaches a password has been involved with in Pasman."}
     ]
 } // this is a HORRID way of doing it. BUT IT WORKS SO FUCK OYU!
 
