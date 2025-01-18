4979deb0570ed6d4aa2a2387fac3a934ec773a1f
Averyden <igo408gt@gmail.com>
2025-01-18 04:16:03 +0100

    animate kaomoji

--- src/scripts/Dependencies/rainbow.ts
+++ src/scripts/Dependencies/rainbow.ts
@@ -1,6 +1,7 @@
 // I know its called rainbow but ill also use this to animate stuff on the site wahoo.
 
-setInterval(rainbow, 1000);
+setInterval(rainbow, 1000)
+setInterval(eepyFrame1, 2000)
 function rainbow() {
     const toColor = document.getElementById('rainbow')
     if (toColor != null) {
@@ -11,10 +12,23 @@ function rainbow() {
 }
 
 function getRandomColor() {
-    let charset = "0123456789ABCDEF";
+    let charset = "0123456789ABCDEF"
     let color = "#"
     for (var i = 0; i<6; i++) {
-        color += charset[Math.floor(Math.random() * 16)];
+        color += charset[Math.floor(Math.random() * 16)]
     }
     return color
 }
+
+
+
+function eepyFrame1() {
+    const eepyElement = document.getElementById('eepy') as HTMLSpanElement
+    eepyElement.innerHTML = "(～﹃～)~zZ"
+}
+
+function eepyFrame2() {
+    const eepyElement = document.getElementById('eepy') as HTMLSpanElement
+    eepyElement.innerHTML = "(～﹃～)~ZzZ"
+}
+
