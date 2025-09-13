aecf8beb95b4b01d36b2df848f7978eca5a0ce95
Averyden <igo408gt@gmail.com>
2025-09-14 00:40:58 +0200

    Implement everything

---
+++ src/components/mainTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <table class="mainBox">
+    <tr>
+      <th>
+        <NavHeader />
+      </th>
+    </tr>
+    <tr>
+      <th>
+        <AboutSection />
+        <hr class="divider" />
+        <ProjectList />
+      </th>
+    </tr>
+  </table>
+</template>
+
+<script setup lang="ts">
+import NavHeader from './NavHeader.vue'
+import ProjectList from './ProjectList.vue'
+import AboutSection from './AboutSection.vue'
+</script>
+
+<style scoped>
+.mainBox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+.divider {
+  color: #41ff41;
+  margin: 10px 0;
+}
+</style>
