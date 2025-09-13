80bde18cae4413868ff204417ecc9feb6fcab0ca
Averyden <igo408gt@gmail.com>
2025-09-14 00:35:08 +0200

    create compoenent

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="nav1" id="info">
+    <h2 class="smallMargin" id="infoTitle">
+      <b style="color: slateblue">about:</b>
+    </h2>
+
+    <div class="subText about-text">
+      &gt; My name is Avery
+      <br />
+      Welcome to my portfolio. <br />
+      currently studying Applied Computer Science (with 11 years of coding experience)<br />
+      <br />
+      &gt; hope youll enjoy lookin around here for now （＾∀＾●）ﾉｼ
+      <br />
+      it must be noted that this is a temporary version of this site until i finish designing
+      everything else!
+    </div>
+
+    <hr class="divider" />
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.about-text {
+  color: #00ffc8;
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.divider {
+  color: #41ff41;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+</style>
